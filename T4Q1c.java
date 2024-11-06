7f7b24a2c29f2a6a2ea53045fff878778bd2fe68
ascensionz04 <yapyh716@gmail.com>
2024-11-06 15:48:10 +0800

    Add files via upload

---
+++ T4Q1c.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.util.Random;
+public class T4Q1c {
+    public static void main(String[] args) {
+        Random r= new Random();
+        int i=0;        
+        for (int n=0;n<4;n++){
+            while (i<5){
+            int mat= r.nextInt(0,101);
+            System.out.print(mat+" ");
+            i=i+1;
+            }
+        System.out.println(" ");
+        i=0;
+        }
+            
+                  
+        
+                 
+        
+        
+    }
+}
